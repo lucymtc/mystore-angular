b255a2eaf5898b702cbd8ed7c54267696a88f5c9
Lucy Tomas <lucy.tomas@get10up.com>
2022-09-26 18:25:20 +0200

    add cart to local storage

--- src/app/services/cart.service.ts
+++ src/app/services/cart.service.ts
@@ -6,22 +6,43 @@ import { CartProduct } from '../models/CartProduct';
   providedIn: 'root'
 })
 export class CartService {
-  cartProductsList: CartProduct[] = [];
+  storageName = 'mystorecart';
   constructor() {}
 
   /**
    * Retrive the list of products added to the cart.
    */
   getCartProducts(): CartProduct[] {
-    return this.cartProductsList;
+    const carStore = window.localStorage.getItem(this.storageName);
+    if (!carStore) {
+      return [];
+    }
+    return JSON.parse(carStore);
   }
 
   /**
-   * Adds Product and Quantity to the cart.
+   * Adds Product and Quantity to the cart or add quantity to existing product.
    */
   addProductToCart(product: Product, quantity: number): CartProduct[] {
-    this.cartProductsList.push({ product, quantity });
-    console.log(this.cartProductsList);
-    return this.cartProductsList;
+    const cartProductsList = this.getCartProducts();
+    console.log(cartProductsList);
+
+    const foundIndex = cartProductsList.findIndex(
+      item => item?.product?.id === product.id
+    );
+
+    if (-1 === foundIndex) {
+      cartProductsList.push({ product, quantity });
+    } else {
+      cartProductsList[foundIndex].quantity =
+        cartProductsList[foundIndex].quantity + quantity;
+    }
+
+    window.localStorage.setItem(
+      this.storageName,
+      JSON.stringify(cartProductsList)
+    );
+
+    return cartProductsList;
   }
 }
